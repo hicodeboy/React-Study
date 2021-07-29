bf7e4f3dd55d27fd84b736d470e2298e501bc908
hicodeboy <2685312867@qq.com>
2021-07-29 22:03:23 +0800

    列表 & key

--- src/index.js
+++ src/index.js
@@ -293,12 +293,80 @@ class Page extends React.Component {
     }
 }
 
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((number) => number * 2);
+console.log(doubled);
+
+function ListItem(props) {
+    return <li>{props.value}</li>
+}
+
+const listItems = numbers.map((number) => 
+    <li>{number}</li>
+);
+
+
+// function NumberList(props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map((number) => 
+//         <li key={number.toString()}>{number}</li>
+//     );
+//     return (
+//         <ul>{listItems}</ul>
+//     );
+// }
+
+function Blog(props) {
+    const sidebar = (
+        <ul>
+            {props.posts.map((post) => 
+            <li key={post.id}>
+                {post.title}
+            </li>
+            )}
+        </ul>
+    );
+    const content = props.posts.map((post) => 
+                <div key={post.id}>
+                    <h3>{post.id}</h3>
+                    <p>{post.content}</p>
+                </div>
+    );
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    );
+}
+
+// const posts = [
+//     {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+//     {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+//   ];
+
+  function NumberList(props) {
+    const numbers = props.numbers;
+    return (
+      <ul>
+        {numbers.map((number) =>
+          <ListItem key={number.toString()}
+                    value={number} />
+        )}
+      </ul>
+    );
+  }
+
 ReactDOM.render(
     // <Clock increment={12}></Clock>,
     // <Form />,
     // <Greeting  isLoggedIn={true}/>,
     // <LoginControl />,
     // <Mailbox unreadMessages={messages}/>,
-    <Page></Page>,
+    // <Page></Page>,
+    // <ul>{listItems}</ul>,
+    <NumberList numbers={numbers} />,
+    // <Blog posts={posts}></Blog> ,
     document.getElementById('root')
 );
