bc7f894551f489eac992ceaa01d3067b6f00ab19
hicodeboy <2685312867@qq.com>
2021-07-30 20:01:26 +0800

    表单

--- src/index.js
+++ src/index.js
@@ -346,27 +346,181 @@ function Blog(props) {
 //     {id: 2, title: 'Installation', content: 'You can install React from npm.'}
 //   ];
 
-  function NumberList(props) {
-    const numbers = props.numbers;
-    return (
-      <ul>
+function NumberList(props) {
+      const numbers = props.numbers;
+   return (
+     <ul>
         {numbers.map((number) =>
           <ListItem key={number.toString()}
                     value={number} />
         )}
       </ul>
     );
-  }
+}
 
-ReactDOM.render(
-    // <Clock increment={12}></Clock>,
-    // <Form />,
-    // <Greeting  isLoggedIn={true}/>,
-    // <LoginControl />,
-    // <Mailbox unreadMessages={messages}/>,
-    // <Page></Page>,
-    // <ul>{listItems}</ul>,
-    <NumberList numbers={numbers} />,
-    // <Blog posts={posts}></Blog> ,
-    document.getElementById('root')
-);
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('提交的名字' + this.state.value);
+        event.preventDefault();
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    名字：
+                    <input type='text' value={this.state.value} onChange={this.handleChange}>
+                    </input>
+                </label>
+                <input type='submit' value="提交"></input>
+            </form>
+        );
+    }
+}
+
+
+class EssayForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '请撰写一篇关于你喜欢的 DOM 元素的文章.'
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('提交的文章 '  + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    文章：
+                    <textarea value={this.state.value} onChange={this.handleChange}></textarea>
+                </label>
+                <input type="submit" value="提交"></input>
+            </form>
+        );
+    }
+}
+
+class FlavorForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'coconut'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('你喜欢的风味是：' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    选择你喜欢的风味：
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="葡萄柚">葡萄柚</option>
+                        <option value="lime">酸橙</option>
+                        <option value="coconut">椰子</option>
+                        <option value="mango">芒果</option>
+                    </select>
+                </label>
+                <input type="submit" value="提交"></input>
+            </form>
+        );
+    }
+}
+
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inGoing: true,
+            numberOfGuests: 2
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type == 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    参与：
+                    <input name="isGoing" type="checkbox" checked={this.state.isGoing}
+                    onChange={this.handleInputChange}>
+                    </input>
+                </label>
+                <br></br>
+                <label>
+                    来宾人数：
+                    <input name="numberOfGuests" 
+                    type="number" 
+                    value={this.state.numberOfGuests} 
+                    onChange={this.handleInputChange}>
+                    </input>
+                </label>
+            </form>
+        );
+    }
+
+
+}
+
+// ReactDOM.render(
+//     // <Clock increment={12}></Clock>,
+//     // <Form />,
+//     // <Greeting  isLoggedIn={true}/>,
+//     // <LoginControl />,
+//     // <Mailbox unreadMessages={messages}/>,
+//     // <Page></Page>,
+//     // <ul>{listItems}</ul>,
+//     // <NumberList numbers={numbers} />,
+//     // <Blog posts={posts}></Blog> ,
+//     // <NameForm></NameForm>,
+//     // <EssayForm></EssayForm>,
+//     // <FlavorForm></FlavorForm>,
+//     <Reservation></Reservation>,
+//     document.getElementById('root')
+// );
+
+// ReactDOM.render(<input value="hi"></input>, document.getElementById('root'));
+setTimeout(() => {
+    ReactDOM.render(<input value={null}></input>, document.getElementById('root'));
+}, 1000);
