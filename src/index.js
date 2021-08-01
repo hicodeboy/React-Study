e1f255f7a77552456d716fea31036c7011bf61bb
hicodeboy <2685312867@qq.com>
2021-08-01 11:43:10 +0800

    组合& 继承

--- src/index.js
+++ src/index.js
@@ -614,6 +614,80 @@ function tryConvert(temperature, convert) {
     return rounded.toString();
 }
 
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+function Dialog(props) {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+            {props.children}
+        </FancyBorder>
+    );
+}
+
+// function WelcomeDialog() {
+//     return (
+//         <FancyBorder color="blue">
+//             <h1 className="Dialog-title">
+//                 Welcome
+//             </h1>
+//             <p className="Dialog-message">
+//                 Thank you for visiting our spacecraft!
+//             </p>
+//         </FancyBorder>
+//     );
+// }
+
+
+  function WelcomeDialog() {
+    return (
+      <Dialog
+        title="Welcome"
+        message="Thank you for visiting our spacecraft!" />
+    );
+  }
+
+  class SignUpDialog extends React.Component {
+      constructor(props) {
+          super(props);
+          this.handleChange = this.handleChange.bind(this);
+          this.handleSignUp = this.handleSignUp.bind(this);
+          this.state = {login: ''}
+      }
+
+      render() {
+          return (
+              <Dialog title="Mars Exploration Program"
+                        message="How should we refer to you?">
+                    <input value={this.state.login}
+                            onChange={this.handleChange}>
+                    </input>
+                    <button onClick={this.handleSignUp}>
+                        Sign Me Up!
+                    </button>
+              </Dialog>
+          );
+      }
+
+      handleChange(e) {
+          this.setState({login: e.target.value});
+      }
+
+      handleSignUp(e) {
+          alert(`Welcome aboard, ${this.state.login}!`);
+      }
+  }
+
 ReactDOM.render(
     // <Clock increment={12}></Clock>,
     // <Form />,
@@ -628,7 +702,9 @@ ReactDOM.render(
     // <EssayForm></EssayForm>,
     // <FlavorForm></FlavorForm>,
     // <Reservation></Reservation>,
-    <Calculator></Calculator>,
+    // <Calculator></Calculator>,
+    // <WelcomeDialog></WelcomeDialog>,
+    <SignUpDialog></SignUpDialog>,
     document.getElementById('root')
 );
 
