4fea6c83411a10949de34fa6f212a2a7b095f903
hicodeboy <2685312867@qq.com>
2021-08-01 11:21:38 +0800

    状态提升

--- src/index.js
+++ src/index.js
@@ -499,28 +499,140 @@ class Reservation extends React.Component {
             </form>
         );
     }
-
-
 }
 
-// ReactDOM.render(
-//     // <Clock increment={12}></Clock>,
-//     // <Form />,
-//     // <Greeting  isLoggedIn={true}/>,
-//     // <LoginControl />,
-//     // <Mailbox unreadMessages={messages}/>,
-//     // <Page></Page>,
-//     // <ul>{listItems}</ul>,
-//     // <NumberList numbers={numbers} />,
-//     // <Blog posts={posts}></Blog> ,
-//     // <NameForm></NameForm>,
-//     // <EssayForm></EssayForm>,
-//     // <FlavorForm></FlavorForm>,
-//     <Reservation></Reservation>,
-//     document.getElementById('root')
-// );
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>
+    }
+    return <p>The water would not boil.</p>
+}
+
+
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        // this.setState({temperature: e.target.value});
+        this.props.onTemperatureChange(e.target.value);
+    }
+
+    render() {
+        // const temperature = this.state.temperature;
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]};</legend>
+                 <input value={temperature} 
+                 onChange={this.handleChange}>
+                </input>
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.handleChange = this.handleChange.bind(this);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {temperature: '', scale: 'c'};
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: 'f', temperature});
+    }
+
+    // handleChange(e) {
+    //     this.setState({temperature: e.target.value});
+    // }
+
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        return (
+            <div>
+                <TemperatureInput scale="c"
+                temperature={celsius}
+                onTemperatureChange={this.handleCelsiusChange}>
+                </TemperatureInput>
+                <TemperatureInput 
+                scale="f" 
+                temperature={fahrenheit}
+                onTemperatureChange={this.handleFahrenheitChange}>
+                </TemperatureInput>
+                <BoilingVerdict celsius={parseFloat(celsius)}></BoilingVerdict>
+            </div>
+        );
+        // return (
+        //     // <fieldset>
+        //     //     <legend>Enter temperature in Celsius;</legend>
+        //     //     <input value={temperature} onChange={this.handleChange}></input>
+        //     //     <BoilingVerdict celsius={parseFloat(temperature)}></BoilingVerdict>
+        //     // </fieldset>
+        //     <div>
+        //         <TemperatureInput scale='c'></TemperatureInput>
+        //         <TemperatureInput scale='f'></TemperatureInput>
+        //     </div>
+        // );
+    }
+}
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+ReactDOM.render(
+    // <Clock increment={12}></Clock>,
+    // <Form />,
+    // <Greeting  isLoggedIn={true}/>,
+    // <LoginControl />,
+    // <Mailbox unreadMessages={messages}/>,
+    // <Page></Page>,
+    // <ul>{listItems}</ul>,
+    // <NumberList numbers={numbers} />,
+    // <Blog posts={posts}></Blog> ,
+    // <NameForm></NameForm>,
+    // <EssayForm></EssayForm>,
+    // <FlavorForm></FlavorForm>,
+    // <Reservation></Reservation>,
+    <Calculator></Calculator>,
+    document.getElementById('root')
+);
 
 // ReactDOM.render(<input value="hi"></input>, document.getElementById('root'));
-setTimeout(() => {
-    ReactDOM.render(<input value={null}></input>, document.getElementById('root'));
-}, 1000);
+// setTimeout(() => {
+//     ReactDOM.render(<input value={null}></input>, document.getElementById('root'));
+// }, 1000);
